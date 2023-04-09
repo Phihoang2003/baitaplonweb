5a16c3cb9b108207b8e059f281fc5753bb0dc311
phihoang <hoangphi1232003@gmail.com>
2023-04-09 20:35:21 +0700

    hoan thanh xong them san pham vao gio hang

---
+++ web/js/cart.js
@@ -0,0 +1,209 @@
+// them san pham
+
+var cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+var tableBody = document.querySelector(".cart-table");
+let totalPrice;
+function getTotalPrice() {
+  let cart = JSON.parse(localStorage.getItem("cartItems")) || [];
+  totalPrice = cart.reduce((total, item) => {
+    return total + item.price * item.quantity;
+  }, 0);
+  return totalPrice;
+}
+getTotalPrice();
+var totalPay = document.querySelector(".total-price");
+totalPay.textContent = totalPrice;
+
+cartItems.forEach(function (item) {
+  var row = document.createElement("tr");
+  var productNameCell = document.createElement("td");
+  var productPriceCell = document.createElement("td");
+  var quantityCell = document.createElement("td");
+  var totalCell = document.createElement("td");
+
+  var Delete = document.createElement("td");
+  productNameCell.innerHTML = item.name;
+  totalCell.innerHTML = Number(item.price) * Number(item.quantity);
+  productPriceCell.innerHTML = item.price;
+  quantityCell.innerHTML = item.quantity;
+  Delete.innerHTML = `<i class="btn-delete fa-solid fa-trash" data-id=${item.id}></i>`;
+  row.appendChild(productNameCell);
+  row.appendChild(quantityCell);
+  row.appendChild(productPriceCell);
+  row.appendChild(totalCell);
+  row.appendChild(Delete);
+  Delete.style.cursor = "pointer";
+  tableBody.appendChild(row);
+});
+// xoa san pham
+
+function renderCart() {
+  var cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+
+  var tableBody = document.querySelector(".cart-table");
+  tableBody.innerHTML = "";
+
+  cartItems.forEach(function (item) {
+    var row = document.createElement("tr");
+
+    var productIdCell = document.createElement("td");
+    productIdCell.innerHTML = item.id;
+    row.appendChild(productIdCell);
+
+    var nameCell = document.createElement("td");
+    nameCell.innerHTML = item.name;
+    row.appendChild(nameCell);
+
+    var priceCell = document.createElement("td");
+    priceCell.innerHTML = item.price;
+    row.appendChild(priceCell);
+
+    var quantityCell = document.createElement("td");
+    quantityCell.innerHTML = item.quantity;
+    row.appendChild(quantityCell);
+
+    var totalCell = document.createElement("td");
+    totalCell.innerHTML = Number(item.price) * Number(item.quantity);
+    row.appendChild(totalCell);
+
+    var removeButtonCell = document.createElement("td");
+    removeButtonCell.innerHTML = `<i class="btn-delete fa-solid fa-trash" data-id=${item.id}></i>`;
+
+    row.appendChild(removeButtonCell);
+
+    tableBody.appendChild(row);
+  });
+}
+// remove
+function removeCartItem(id) {
+  var cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+  var index = -1;
+
+  // Tìm kiếm sản phẩm cần xóa trong mảng các sản phẩm trong giỏ hàng
+  for (var i = 0; i < cartItems.length; i++) {
+    if (cartItems[i].id === id) {
+      index = i;
+      break;
+    }
+  }
+
+  if (index !== -1) {
+    // Nếu sản phẩm được tìm thấy, xóa sản phẩm đó khỏi mảng
+    if (cartItems[index].quantity > 1) {
+      cartItems[index].quantity = cartItems[index].quantity - 1;
+    } else {
+      cartItems.splice(index, 1);
+    }
+
+    // Cập nhật lại giá trị của giỏ hàng trong localStorage
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    location.reload();
+
+    // Cập nhật lại giao diện của giỏ hàng bằng cách render lại các sản phẩm còn lại trong giỏ hàng
+    renderCart();
+  }
+}
+
+document.querySelectorAll(".btn-delete").forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    var productId = this.dataset.id;
+    removeCartItem(productId);
+  });
+});
+
+// cachkhac
+
+$(document).ready(function () {
+  var i = 1;
+  $("#btnModal").click(function () {
+    $("#myModal").modal();
+  });
+  $("#myModal").modal("hide");
+  $("#luu").click(function () {
+    var ten = $("#txtTen").val();
+    var dc = $("#txtDC").val();
+    var ngay = $("#txtNgay").val();
+    var gio = $("#txtTime").val();
+    var email = $("#txtEmail").val();
+    var dt = $("#txtdt").val();
+
+    var them =
+      "<tr><td>" +
+      i++ +
+      "</td><td>" +
+      ten +
+      "</td><td>" +
+      dc +
+      "</td><td>" +
+      ngay +
+      "</td><td>" +
+      gio +
+      "</td><td>" +
+      email;
+    +"</td><td>" + dt + "</td><>";
+    ("</td></tr>");
+
+    $("table tbody").append(them);
+  });
+  function kiemTraTen() {
+    var kt = /^[A-Z][a-zA-Z]+(\s[A-Z][a-zA-Z]+)*$/;
+    ten = $("#txtTen").val();
+    if (ten == "") {
+      $("#loiTen").html("Bắt buộc nhập");
+      return false;
+    }
+    if (!kt.test(ten)) {
+      $("#loiTen").html(" *Chữ cái đầu của mỗi từ phải viết hoa !!");
+      return false;
+    }
+    $("#loiTen").html("*");
+    return true;
+  }
+  $("#txtTen").blur(kiemTraTen);
+  function kiemTraDT() {
+    var kt = /^[0-9]{4}\-[0-9]{3}\-[0-9]{3}$/;
+    dt = $("#txtDT").val();
+    if (dt == "") {
+      $("#loiDT").html("Bắc buộc nhập");
+      return false;
+    }
+    if (!kt.test(dt)) {
+      $("#loiDT").html("Nhập đúng mẫu: 0000-000-000");
+      return false;
+    }
+    $("#loiDT").html("*");
+    return true;
+  }
+  $("#txtDT").blur(kiemTraDT);
+  function kiemTraDC() {
+    var kt = /^[A-Z][a-zA-Z]+(\s[A-Z][a-zA-Z]+)*$/;
+    ten = $("#txtTen").val();
+    if (ten == "") {
+      $("#loiDC").html("Bắt buộc nhập");
+      return false;
+    }
+    if (!kt.test(ten)) {
+      $("#loiDC").html(" *Chữ cái đầu của mỗi từ phải viết hoa !!");
+      return false;
+    }
+    $("#loiDCx").html("*");
+    return true;
+  }
+  $("#txtDC").blur(kiemTraDC);
+  function kiemTraNgay() {
+    var ngay = $("#txtNgay").val();
+    if (ngay == "") {
+      $("#loiNgay").html("Bắc buộc nhập");
+      return false;
+    }
+    day = new Date(ngay);
+    today = new Date();
+    if (day < today) {
+      $("#loiNgay").html("Ngày phải sau ngày hiện tại");
+      return false;
+    }
+    $("#loiNgay").html("*");
+    return true;
+  }
+  $("#txtNgay").blur(kiemTraNgay);
+});
--- web/js/home.js
+++ web/js/home.js
@@ -1,14 +1,41 @@
-// var shopGift = document.querySelector("#shop-gift");
+var addToCartButton = document.querySelectorAll(".add-to-cart-btn");
+addToCartButton.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    var productId = e.target
+      .closest(".item-watch-forher")
+      .getAttribute("data-product-id");
+    var productName =
+      button.parentElement.querySelector(".product-name").innerText;
+    var productPrice =
+      button.parentElement.querySelector(".product-price").innerText;
 
-// shopGift.addEventListener("click", function () {
-//   // Mở trang HTML khác
-//   window.location.href = "../for-her/forher.html";
-// });
+    var product = {
+      id: productId,
+      name: productName,
+      price: productPrice,
+    };
 
-// var watchs = document.querySelector("#watchs");
-// console.log(shopGift);
+    // Lấy danh sách sản phẩm trong giỏ hàng từ Local Storage
+    var cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
 
-// shopGift.addEventListener("click", function () {
-//   // Mở trang HTML khác
-//   window.location.href = "../for-her/forher.html";
-// });
+    // Kiểm tra xem sản phẩm đã có trong giỏ hàng chưa
+    var itemIndex = cartItems.findIndex(function (item) {
+      return item.id === productId;
+    });
+
+    if (itemIndex !== -1) {
+      // Nếu sản phẩm đã có trong giỏ hàng, tăng số lượng sản phẩm
+      cartItems[itemIndex].quantity += 1;
+    } else {
+      // Nếu sản phẩm chưa có trong giỏ hàng, thêm sản phẩm vào giỏ hàng
+      product.quantity = 1;
+      cartItems.push(product);
+    }
+
+    // Lưu danh sách sản phẩm vào Local Storage
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+
+    // Hiển thị thông báo cho người dùng biết sản phẩm đã được thêm vào giỏ hàng
+    alert("Sản phẩm đã được thêm vào giỏ hàng.");
+  });
+});
