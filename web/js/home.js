8ca7345f26afeef8be5d95c1e0a95018a3099da8
phihoang <hoangphi1232003@gmail.com>
2023-04-09 09:22:38 +0700

    hoan thanh xong for him page

---
+++ web/js/home.js
@@ -0,0 +1,14 @@
+// var shopGift = document.querySelector("#shop-gift");
+
+// shopGift.addEventListener("click", function () {
+//   // Mở trang HTML khác
+//   window.location.href = "../for-her/forher.html";
+// });
+
+// var watchs = document.querySelector("#watchs");
+// console.log(shopGift);
+
+// shopGift.addEventListener("click", function () {
+//   // Mở trang HTML khác
+//   window.location.href = "../for-her/forher.html";
+// });
