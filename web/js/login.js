4d2d1b086dfd038cf6b6769ac1cba4192ae96f89
phihoang <hoangphi1232003@gmail.com>
2023-04-10 08:39:14 +0700

    hoan thanh logic dang ki

--- web/js/login.js
+++ web/js/login.js
@@ -1,80 +1,3 @@
-var x = document.getElementById("login");
-var y = document.getElementById("register");
-var z = document.getElementById("btn");
-
-function register() {
-  x.style.left = "-400px";
-  y.style.left = "50px";
-  z.style.left = "110px";
-}
-
-function login() {
-  x.style.left = "50px";
-  y.style.left = "450px";
-  z.style.left = "0";
-}
-
-function registertest() {
-  var id = document.getElementById("userid").value;
-  var password = document.getElementById("pwd").value;
-  var Email = document.getElementById("email").value;
-  var confirmpwd = document.getElementById("cf-pwd").value;
-
-  localStorage.getItem("userid", id);
-  localStorage.getItem("pwd", password);
-  localStorage.getItem("email", Email);
-
-  //   return true;
-
-  let useridRe = document.getElementById("useridRe").value;
-  if (userid.trim() == "") {
-    alert("tên người dùng không được để trống");
-    return false;
-  }
-  let email = document.getElementById("email").value;
-  if (email.trim() == "") {
-    alert("email không được đẻ trống");
-    return false;
-  }
-  let pwdRe = document.getElementById("pwdRe").value;
-  if (pwd.trim() == "") {
-    alert("Bạn phải nhập mật khẩu!");
-    return false;
-  }
-  let confirmpassword = document.getElementById("confirmpassword").value;
-  if (pwdRe != confirmpassword) {
-    alert("Mật khẩu không khớp, vui lòng nhập lại");
-    return false;
-  }
-  alert("Đăng ký thành công!");
-  return true;
-}
-
-$(document).ready(function () {
-  $("#register").submit(function () {
-    var useridRegister = $("#useridRe").val();
-    var pwdRegister = $("#pwdRe").val();
-    var emailRegister = $("#email").val();
-    localStorage.setItem("useridRe", useridRegister);
-    localStorage.setItem("pwdRe", pwdRegister);
-    localStorage.setItem("email", emailRegister);
-  });
-
-  $("#login").submit(function () {
-    var enteredUserid = $("#useridLog").val();
-    var enteredPass = $("#pwdLog").val();
-
-    var storedUserid = localStorage.getItem("useridRe");
-    var storedPass = localStorage.getItem("pwdRe");
-
-    if (enteredUserid == storedUserid && enteredPass == storedPass) {
-      alert("You are logged in!");
-    } else {
-      alert("Username and Password do not match!");
-    }
-  });
-});
-//
 var emailArray = [];
 var passwordArray = [];
 
@@ -85,8 +8,8 @@ var forgetBox = document.getElementById("forgot");
 var loginTab = document.getElementById("lt");
 var regTab = document.getElementById("rt");
 
-function regTabFun(e) {
-  e.preventDefault();
+function regTabFun() {
+  event.preventDefault();
 
   regBox.style.visibility = "visible";
   loginBox.style.visibility = "hidden";
@@ -95,8 +18,8 @@ function regTabFun(e) {
   regTab.style.backgroundColor = "rgb(12, 132, 189)";
   loginTab.style.backgroundColor = "rgba(11, 177, 224, 0.82)";
 }
-function loginTabFun(e) {
-  e.preventDefault();
+function loginTabFun() {
+  event.preventDefault();
 
   regBox.style.visibility = "hidden";
   loginBox.style.visibility = "visible";
@@ -105,8 +28,8 @@ function loginTabFun(e) {
   loginTab.style.backgroundColor = "rgb(12, 132, 189)";
   regTab.style.backgroundColor = "rgba(11, 177, 224, 0.82)";
 }
-function forTabFun(e) {
-  e.preventDefault();
+function forTabFun() {
+  event.preventDefault();
 
   regBox.style.visibility = "hidden";
   loginBox.style.visibility = "hidden";
@@ -147,8 +70,8 @@ function register() {
     return;
   }
 }
-function login(e) {
-  e.preventDefault();
+function login() {
+  event.preventDefault();
 
   var email = document.getElementById("se").value;
   var password = document.getElementById("sp").value;
@@ -177,8 +100,8 @@ function login(e) {
     return;
   }
 }
-function forgot(e) {
-  e.preventDefault();
+function forgot() {
+  event.preventDefault();
 
   var email = document.getElementById("fe").value;
 
@@ -194,19 +117,3 @@ function forgot(e) {
   alert("email is send to your email check it in 24hr. \n Thanks");
   document.getElementById("fe").value = "";
 }
-
-//
-
-const navbar = document.querySelector(".bar");
-const nav = document.querySelector(".navbar");
-const close = document.querySelector(".close");
-
-console.log(navbar);
-
-navbar.addEventListener("click", function () {
-  nav.classList.add("active");
-});
-
-close.addEventListener("click", function () {
-  nav.classList.remove("active");
-});
